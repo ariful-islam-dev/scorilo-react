dd7ca067927ac615259e8ff977cf6d95d5b1e31d
ariful4082 <ariful4082@gmail.com>
2021-04-04 01:04:36 +0600

    manage page route

---
+++ src/Pages/404.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div className="">
+            <div className="container text-center">
+                <h1 className="display-4">404</h1>
+                <p>Page Not Found <a href="/">Home</a></p>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound
