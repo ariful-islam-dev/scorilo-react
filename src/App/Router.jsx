2ea4bf1fb392336fa8d34b54d6f5dea4f24a7bff
ariful4082 <ariful4082@gmail.com>
2021-04-05 14:52:05 +0600

    complet checkout page

--- src/App/Router.jsx
+++ src/App/Router.jsx
@@ -1,6 +1,6 @@
 import { Router } from "@reach/router"
-import Product from "../Components/Product"
 import NotFound from "../Pages/404"
+import CheckoutPage from "../Pages/CheckoutPage"
 import Contact from "../Pages/Contact"
 import Home from "../Pages/Home"
 import ProductPage from "../Pages/ProductPage"
@@ -12,6 +12,7 @@ const AppRouter = () => {
            <Shop path="/shop" />
            <Contact path="/contact" />
            <ProductPage path="/product" />
+           <CheckoutPage path="/checkout" />
            <NotFound default />
        </Router> 
     )
